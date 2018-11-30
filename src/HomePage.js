080dd7c7cc44fbd7d32cb67efe0414171d0977c5
kwu1224 <kwu1224@hotmail.com>
2018-11-29 21:28:30 -0800

    part1

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,12 +1,74 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
+import {loadAllMessages} from './redux/actions';
+
+class Message extends Component{
+    render(){
+        return (
+            <div className="message">
+                {this.props.content}
+            </div>
+        );
+    }
+}
 
 class HomePage extends Component {
+    constructor(props){
+        super(props);
+        this.sendSomeData = this.sendSomeData.bind(this);
+        this.updateMessage = this.updateMessage.bind(this);
+        this.state = {
+            content: null,
+            messageValue: '',
+        };
+    }
+
+    componentDidMount(){
+        axios.get('/api/messages')
+        .then((res) => {
+            console.log(res.data)
+        }).catch((e) => {
+            console.log(e)
+        });
+    }
+
+    updateMessage(e){
+        this.setState({
+            messageValue: e.target.value,
+        });
+    }
+
+    sendSomeData(){
+        axios({
+            method: 'POST',
+            url: '/api/sendmessage',
+            data: {
+                message: this.state.messageValue
+            }
+        })
+    .then((res) => {
+        console.log(res);
+    }).catch((e) => {
+        console.log(e);
+    });
+    
+    }
+
     render() {
         return (
             <div>
                 <h1>Home Page</h1>
+                <div classname ="messages">
+                {
+                    this.props.messages.map((messageData, i) => {
+                        return <Message key ={i} connect={messageData}/>
+                    })
+                }
+                </div>
+                <input value={this.state.messageValue} onChange={this.updatemessage}/>
+                <button onClick ={this.sendSomeData}>  Send some post data   </button>
                 <p>
                     Here is my main page content <Link to="/page1/mail">Mail</Link>
                 </p>
@@ -20,10 +82,12 @@ class HomePage extends Component {
 
 const mapStateToProps = (state, ownProps) => {
     return {
+        messages : state.testReducer.messages,
     };
 };
 
-const mapDispatchToProps = {};
+
+const mapDispatchToProps = {loadAllMessages};
 
 export default connect(
     mapStateToProps,
--- src/redux/testReducer.js
+++ src/redux/testReducer.js
@@ -1,9 +1,20 @@
 const initialState = {
     test: 'test value',
+    messages: ['testing'],
 };
 
 const testReducer = (state = initialState, action) => {
     switch (action.type) {
+        case 'LOAD_MESSAGES':
+            return Object.assign({}, state, {
+                messages: action.messages,
+            });
+        case 'IMPORT_MESSAGES':
+            const newMessages = state.messages.slice(0);
+            newMessages.unshift(action.message);
+            return Object.assign({}, state, {
+                messages: newMessages,
+            });
         case 'SET_TEST':
             return Object.assign({}, state, {
                 test: action.test
